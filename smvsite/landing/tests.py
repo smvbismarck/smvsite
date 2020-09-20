96119471875b2721a2c51512e26b4ec7cd855d7c
Yannis Storrer <yannis.storrer@web.de>
2020-09-20 16:08:50 +0200

    Added tests for the landing page #17

--- smvsite/landing/tests.py
+++ smvsite/landing/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
+
+class PageTests(TestCase):
+
+    def test_root_status_code(self):
+        response = self.client.get("/")
+        self.assertEquals(response.status_code, 200)
+
+    def test_root_content(self):
+        response = self.client.get("/")
+        self.assertEquals(response.content, b"Hello World")
+
 # Create your tests here.
