a86bb8bdc0daaff7e21edc28f788192ca070a60e
Yannis Storrer <yannis.storrer@web.de>
2020-09-21 07:16:56 +0200

    Added komitee database models #5

--- smvsite/landing/models.py
+++ smvsite/landing/models.py
@@ -1,1 +1,10 @@
-# Create your models here.
+from django.db import models
+
+
+class Komitee(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=200)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
--- smvsite/smvsite/settings.py
+++ smvsite/smvsite/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'landing'
+    'landing',
+    'komitee'
 ]
 
 MIDDLEWARE = [
