5ebcf29cd240c31247e9c9f8d74632051d9ec029
Yannis Storrer <yannis.storrer@web.de>
2020-09-28 19:37:06 +0200

    Added dropdown for page root

--- smvsite/komitee/urls.py
+++ smvsite/komitee/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:article_name>', views.blogEntry, name='index'),
+    path('<str:article_name>', views.blogEntry, name='komitee'),
 ]
