e7271010c050cd42ef6d2005cc304f1413951bb5
Yannis Storrer <yannis.storrer@web.de>
2020-09-29 18:48:14 +0200

    Added tests for the komitee view

--- smvsite/blog/views.py
+++ smvsite/blog/views.py
@@ -4,5 +4,5 @@ from .models import Article
 
 def blogEntry(request, article_id):
     articles = Article.objects.all()
-    blog_article = get_object_or_404(Article, id=article_id)
+    blog_article = get_object_or_404(Article, id=article_id, is_post=True)
     return render(request, "detail.html", {"article": blog_article, "articles": articles})
--- smvsite/komitee/views.py
+++ smvsite/komitee/views.py
@@ -3,6 +3,6 @@ from blog import models as blog_models
 
 
 def blogEntry(request, article_name):
-    komitee_article = get_object_or_404(blog_models.Article, title=article_name)
+    komitee_article = get_object_or_404(blog_models.Article, title=article_name, is_post=False)
     articles = blog_models.Article.objects.all()
     return render(request, "detail.html", {"article": komitee_article, "articles": articles})
