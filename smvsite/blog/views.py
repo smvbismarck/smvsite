4ac423f48001eb9757ff94387079d3f528a3fda8
Yannis Storrer <yannis.storrer@web.de>
2020-09-29 15:05:36 +0200

    Enabled template for the komitee views

--- smvsite/blog/views.py
+++ smvsite/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Article
+from komitee import models as komitee_models
 
 
 def blogEntry(request, article_id):
+    komitee_list = komitee_models.Komitee.objects.all()
     blog_article = get_object_or_404(Article, id=article_id)
-    return render(request, "detail.html", {"article": blog_article})
+    return render(request, "detail.html", {"article": blog_article, "komitees": komitee_list})
--- smvsite/komitee/views.py
+++ smvsite/komitee/views.py
@@ -1,9 +1,8 @@
-from django.http import HttpResponse
-import html
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from .models import Komitee
 
 
 def blogEntry(request, article_name):
     komitee_article = get_object_or_404(Komitee, title=article_name)
-    return HttpResponse(html.escape(komitee_article.body))
+    komitee_list = Komitee.objects.all()
+    return render(request, "detail.html", {"article": komitee_article, "komitees": komitee_list})
