a7c39071bb260219a3a7edd5ac35ed8c8a7afdbf
Yannis Storrer <yannis.storrer@web.de>
2020-09-29 19:22:39 +0200

    Added basic markdown parser

--- smvsite/blog/views.py
+++ smvsite/blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Article
+from mistune import markdown
 
 
 def blogEntry(request, article_id):
     articles = Article.objects.all()
     blog_article = get_object_or_404(Article, id=article_id, is_post=True)
+    blog_article.body = markdown(blog_article.body)
     return render(request, "detail.html", {"article": blog_article, "articles": articles})
