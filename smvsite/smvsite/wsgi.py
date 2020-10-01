68c80695aa0fc6d883c789b68a49d331b3371e7c
Yannis Storrer <yannis.storrer@web.de>
2020-10-01 22:11:41 +0200

    Added staticfiles storage

--- smvsite/smvsite/wsgi.py
+++ smvsite/smvsite/wsgi.py
@@ -11,6 +11,7 @@ import os
 from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+from django.conf import settings
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smvsite.settings')
@@ -18,3 +19,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smvsite.settings')
 application = get_wsgi_application()
 application = WhiteNoise(application)
 application.add_files(os.path.join(BASE_DIR, "static"), prefix='static/')
+
+if not settings.DEBUG:
+    application.add_files(os.path.join(BASE_DIR, "media"), prefix="media/")
