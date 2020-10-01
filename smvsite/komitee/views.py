51a6128efb5df21bcb6654e2d720473aa2450416
Yannis Storrer <yannis.storrer@web.de>
2020-10-01 14:53:52 +0200

    Added basic redis cache

--- smvsite/komitee/views.py
+++ smvsite/komitee/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import get_object_or_404, render
 from blog import models as blog_models
 from mistune import markdown
+from django.views.decorators.cache import cache_page
 
 
+@cache_page(30)
 def blogEntry(request, article_name):
     komitee_article = get_object_or_404(blog_models.Article, title=article_name, is_post=False)
     komitee_article.body = markdown(komitee_article.body, bodyescape=True, hard_wrap=True)
